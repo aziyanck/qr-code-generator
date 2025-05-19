bb9af9c85527fcfd905a9c16c566ad759cb7f249
aziyanck <aziyan916@gmail.com>
2025-05-19 19:51:42 +0530

    Initialization

---
+++ script.js
@@ -0,0 +1,10 @@
+const url  = document.getElementById('name');
+const btn  = document.getElementById('generate');
+
+
+
+btn.addEventListener("click", function(){
+    console.log("clicked");
+    console.log(url.value);
+});
+
